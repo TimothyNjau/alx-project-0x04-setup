cc897f5e221ce034a94d5d4ba54cb4bbbfa32d76
T1mothy Nj4u <93652350+TimothyNjau@users.noreply.github.com>
2025-07-31 16:48:44 +0300

    feat: Include a ContextAPU for global state management

---
+++ alx-project-0x05/components/layouts/Layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+import Header from "./Header";
+
+
+
+const Layout = ({children}: {children: ReactNode}) => {
+
+    return (
+        <>
+            <Header />
+            {children}
+        </>
+    )
+}
+
+export default Layout;
